603811721876aa39697fc202c7d3f3d358c435fa
insight <inSight01@users.noreply.github.com>
2015-07-05 17:34:45 -0700

    Added Project

---
+++ Project/desktop/src/com/insight/project/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.insight.project.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.insight.project.Project;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Project(), config);
+	}
+}
